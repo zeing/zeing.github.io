c1c019b9188d6a7339d17e964d63f0720942db15
Witthawin Sirisiwaphak <eing.wi@gettgo.com>
2021-01-11 23:47:21 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-63771ca75d72f638fb2c.js"
+    "url": "webpack-runtime-1965cac2377b7a91975d.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "styles-755093da0c07f4b49226.js"
   },
   {
-    "url": "app-3a64b9191bbcd92fdf24.js"
+    "url": "app-e11a615e928197a5d667.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4040094856cf57d2d836d95f878180b3"
+    "revision": "8fa3fef4f3920cee0df406bacdd31fef"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
@@ -145,7 +145,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-3a64b9191bbcd92fdf24.js`))) {
+  if (!resources || !(await caches.match(`/app-e11a615e928197a5d667.js`))) {
     return await fetch(event.request)
   }
 

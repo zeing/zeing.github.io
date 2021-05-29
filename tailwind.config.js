67abc06314713a83306cf027ff4985f001ec1d46
Eing <zeing@outlook.com>
2021-05-30 00:39:57 +0700

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
-      display: ["Prompt", "sans-serif"],
-      body: ["Prompt", "sans-serif"],
+      display: ['Prompt', 'sans-serif'],
+      body: ['Prompt', 'sans-serif'],
     },
     extend: {},
   },
   variants: {
     extend: {
-      fontSize: ["hover", "focus"],
-      scale: ["active", "group-hover"],
+      fontSize: ['hover', 'focus'],
+      scale: ['active', 'group-hover'],
       opacity: ['group-hover'],
       backgroundOpacity: ['group-hover'],
     },
